8074debabdfd2a522b13722840374786fc2cd67d
nick <nickshelenin@gmail.com>
2020-02-29 23:42:58 +0600

    index page changes

--- js/main.js
+++ js/main.js
@@ -30,30 +30,3 @@ for (let i = 0; i < switchLinks.length; i++) {
   });
 }
 
-// testimonial slider
-
-  let slideIndex = 1;
-  showSlides(slideIndex);
-
-  function currentSlide(n) {
-    showSlides((slideIndex = n));
-  }
-
-  function showSlides(n) {
-    var slides = document.getElementsByClassName('figure');
-    var dots = document.getElementsByClassName('testimonial-dot');
-    if (n > slides.length) {
-      slideIndex = 1;
-    }
-    if (n < 1) {
-      slideIndex = slides.length;
-    }
-    for (let i = 0; i < slides.length; i++) {
-      slides[i].style.display = 'none';
-    }
-    for (let i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(' selected', '');
-    }
-    slides[slideIndex - 1].style.display = 'block';
-    dots[slideIndex - 1].className += ' selected';
-  }
