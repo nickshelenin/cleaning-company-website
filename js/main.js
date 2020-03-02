2db064cea2312992a951b00e0acc9efdf1d41154
nick <nickshelenin@gmail.com>
2020-03-02 14:51:05 +0600

    intial commit

--- js/main.js
+++ js/main.js
@@ -1,32 +1,31 @@
 // switch links
 
-const switchLinks = document.querySelectorAll('.switch-link');
-const infoSection = document.querySelectorAll('.info-section');
+const switchLinks = document.querySelectorAll(".switch-link");
+const infoSection = document.querySelectorAll(".info-section");
 
 for (let i = 0; i < switchLinks.length; i++) {
-  switchLinks[i].addEventListener('click', (e) => {
-    switchLinks.forEach((el) => {
-      el.classList.remove('selected-link');
-    });
+   switchLinks[i].addEventListener("click", e => {
+      switchLinks.forEach(el => {
+         el.classList.remove("selected-link");
+      });
 
-    infoSection.forEach((el) => {
-      el.style.display = 'none';
-    });
+      infoSection.forEach(el => {
+         el.style.display = "none";
+      });
 
-    if (switchLinks[i].classList.contains('switch-link-1')) {
-      switchLinks[i].classList.add('selected-link');
-      infoSection[i].style.display = 'block';
-    }
+      if (switchLinks[i].classList.contains("switch-link-1")) {
+         switchLinks[i].classList.add("selected-link");
+         infoSection[i].style.display = "block";
+      }
 
-    if (switchLinks[i].classList.contains('switch-link-2')) {
-      switchLinks[i].classList.add('selected-link');
-      infoSection[i].style.display = 'block';
-    }
+      if (switchLinks[i].classList.contains("switch-link-2")) {
+         switchLinks[i].classList.add("selected-link");
+         infoSection[i].style.display = "block";
+      }
 
-    if (switchLinks[i].classList.contains('switch-link-3')) {
-      switchLinks[i].classList.add('selected-link');
-      infoSection[i].style.display = 'block';
-    }
-  });
+      if (switchLinks[i].classList.contains("switch-link-3")) {
+         switchLinks[i].classList.add("selected-link");
+         infoSection[i].style.display = "block";
+      }
+   });
 }
-
